9ae6719ba0a0ae5a7ef041c869ea6ee2ab2d6a37
JhaAakri <aditijha0830@gmail.com>
2023-08-15 07:11:56 +0530

    Changes updated

---
+++ Myntra-clone/Common Files/women.js
@@ -0,0 +1,94 @@
+
+// <!-- CATEGORIES DISPALY 1 -->
+var categoryBag = [
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2023/Augart23/augart23sbc/1._SY530_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2023/Augart23/augart23sbc/2._SY530_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2023/Augart23/augart23sbc/6._SY530_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2023/Augart23/augart23sbc/4._SY530_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2023/Augart23/augart23sbc/3._SY530_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2023/Augart23/augart23sbc/7._SY530_QL85_FMpng_.png'},
+]
+categoryBag.map(function(element){
+  var box = document.createElement('div');
+  var img =  document.createElement('img');
+  img.setAttribute('src', element.image_url)
+  box.append(img); 
+  document.getElementById('categoryBag_women').append(box)
+})
+
+// <!-- BRAND DISPLAY 2 -->
+var brandsBag = [
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2020/WRS23/Westernbrands/Levi-s._SX564_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2020/WRS23/Westernbrands/ONLY._SX564_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2020/WRS23/Westernbrands/Zink-london._SX564_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2020/WRS23/Westernbrands/Allen-Solly._SX564_QL85_FMpng_.png'},
+  {image_url :'https://m.media-amazon.com/images/G/31/img2020/fashion/WA_2020/WRS23/Westernbrands/AND._SX564_QL85_FMpng_.png'},
+]
+brandsBag.map(function(element){
+  var box = document.createElement('div');
+  var img =  document.createElement('img');
+  img.setAttribute('src', element.image_url)
+  box.append(img); 
+  document.getElementById('explore_more_women').append(box)
+})
+
+
+// <------- SLIDER IMAGES -------> //
+var slideImages = [
+  //FOR TABLET & DEKSTOP VIEW
+  [{img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/N2GLoffer/welcomepc-V1._CB587586825_.gif"},
+  {img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/postpd/headers/unrec-eng-pc._SX3000_QL85_.jpg"},
+  {img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/pc/CML/CML-4._SX3000_QL85_.jpg"},
+  {img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/pc/CML/CML-1._SX3000_QL85_.jpg"},
+  ],
+
+  //FOR MOBILE VIEW
+  [{img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/N2GLoffer/welcomepc-V1._CB587586825_.gif"},
+  {img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/postpd/headers/unrec-eng-pc._SX3000_QL85_.jpg"},
+  {img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/pc/CML/CML-4._SX3000_QL85_.jpg"},
+  {img_url : "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/pc/CML/CML-1._SX3000_QL85_.jpg"},
+  ]
+]
+
+var z = null; 
+
+  //script for media queries
+  var value = window.matchMedia("(max-width: 550px)") //FOR TABLET VIEW
+    mFunction(value) 
+    value.addListener(mFunction) 
+    function mFunction(value) {
+      if (value.matches) { 
+        z = slideImages[1];
+      } 
+      else {
+        z = slideImages[0]
+      }
+  }
+
+z.map(function(ele, index){
+  var slideDiv = document.createElement('div');
+  switch(index){
+    case 0 :
+      slideDiv.setAttribute('class', 'slide first');
+      break;
+    default :
+    slideDiv.setAttribute('class', 'slide')
+  }
+ 
+  var images = document.createElement('img');
+  images.src = ele.img_url;
+  
+  slideDiv.append(images); 
+  document.getElementById('slides').append(slideDiv)
+})
+
+var images = document.querySelectorAll('img')
+for(let i=0; i<images.length; i++){
+  images[i].addEventListener('click', function(){
+    window.location.href = "../womensdata/women.html"
+  })
+}
+// LANDING PAGE
+document.getElementById('landingPage').addEventListener('click', function(){
+  window.location.href = "MainPage.html"
+})
---
+++ Myntra-clone/menspage/mens.js
@@ -0,0 +1,512 @@
+var productData = [{
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2275365/2022/11/22/a3af8a2f-a385-4cb7-bf7b-e34e0925fe0d1669105782439-Roadster-Men-White--Pure-Cotton-T-shirt-7301669105781913-1.jpg",
+    brand: "Roadster",
+    para: "Men White Pure Cotton T-shirt",
+    price: "Rs. 284",
+    rs: 284,
+    strikedoffprice: "Rs 499",
+    offer: "(43% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/6796854/2018/8/7/390e71e8-6e3a-42c9-9b46-14219e6d00eb1533641218049-Louis-Philippe-Men-Sea-Green-Solid-Polo-Collar-T-shirt-52915-1.jpg",
+    brand: "Louis Philippe",
+    para: "Men Teal Green Polo Collar T-shirt",
+    price: "Rs. 1329",
+    rs: 1329,
+    strikedoffprice: "Rs 1899",
+    offer: "(30% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13640716/2021/5/24/48e9e3bc-8fb7-4af5-97fa-46927f68fbd81621838454680JeansRoadsterMenJeansRoadsterMenTrackPantsHRXbyHrithikRoshan1.jpg ",
+    brand: "U.S. Polo Assn.",
+    para: "Printed Polo Collar Pure Cotton T-Shirt",
+    price: "Rs. 1259",
+    rs: 1259,
+    strikedoffprice: "Rs 2099",
+    offer: "(40% OFF)",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "Tshirts",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10381447/2022/4/18/ecd777c3-d057-420f-9a09-b3a0daa69cf21650284975539CampusSutraMenBlackGreyColourblockedHoodedSweatshirt1.jpg",
+    brand: "Campus Sutra",
+    para: "Men Black & Grey Colourblocked Hooded Sweatshirt",
+    price: "Rs. 728",
+    rs: 728,
+    strikedoffprice: "Rs 1999 ",
+    offer: "(Rs. 171 OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21164476/2022/12/12/6fee514e-2c6c-4116-b121-49ec678958821670846783167KISAHMenPinkAnimalPrintedKurtawithDhotiPants1.jpg",
+    brand: "Jompers",
+    para: "Men Printed Thong Flip-Flops ",
+    price: "Rs. 599 ",
+    rs: 599,
+    strikedoffprice: "Rs 999 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/20882622/2022/11/23/8c7d3b74-a2c8-4568-b4e2-2121497673d21669206767082MensEmbroideredKurtas1.jpg",
+    brand: "Jompers",
+    para: "Men Ethnic Motifs Mirror Work Indigo Cotton Kurta",
+    price: "Rs. 1418 ",
+    rs: 1418,
+    strikedoffprice: "Rs 4299 ",
+    offer: "(67% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10777802/2019/10/19/c98a4fdf-8c5e-48a3-87e8-3fcea320e44d1571467255642-MANQ-Men-Trousers-3321571467253413-1.jpg ",
+    brand: "Van Heusen",
+    para: "Men Formal Trousers ",
+    price: "Rs. 1799 ",
+    rs: 1799,
+    strikedoffprice: "Rs 2999 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/23237896/2023/7/10/8f581bfe-1469-40ab-8623-85997cea1c071688965274055-Raymond-Men-Trousers-8561688965272480-1.jpg",
+    brand: "Van Heusen",
+    para: "Men Formal Trousers",
+    price: "Rs. 1799 ",
+    rs: 1799,
+    strikedoffprice: "Rs 2999 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/23262422/2023/5/18/ecb8cc2f-0cbb-476c-b6f9-51aed1c20e3e1684426637824SportsShoes1.jpg",
+    brand: "HRX by Hrithik Roshan",
+    para: "Men Memory Foam Mesh Non-Marking Running Shoes",
+    price: "Rs. 1999 ",
+    rs: 1999,
+    strikedoffprice: "Rs 3999 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url: "https://assets.myntassets.com/assets/images/11552312/2022/5/18/c83aff42-7a36-4879-94ff-29ad1d4e15361652872842945-HRX-by-Hrithik-Roshan-Men-Blue-Solid-Metaflash-Running-Shoes-1.jpg",
+    brand: "HRX by Hrithik Roshan",
+    para: "Men Memory Foam Mesh Non-Marking Running Shoes ",
+    price: "Rs. 1799 ",
+    rs: 1799,
+    strikedoffprice: "Rs 2999 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/23262420/2023/5/18/3badd220-3fa0-41fe-8a78-44fb116dc32f1684426622246SportsShoes1.jpg",
+    brand: "HRX by Hrithik Roshan",
+    para: "Men Solid CARLIN Sneakers ",
+    price: "Rs. 1559 ",
+    rs: 1559,
+    strikedoffprice: "Rs 2599 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/11261278/2023/4/21/ca7de421-3d24-4c1b-a922-5641a2f062f61682057816466-HRX-by-Hrithik-Roshan-Men-White-Solid-Gamescape-Sneakers-929-1.jpg",
+    brand: "HRX by Hrithik Roshan",
+    para: "Men Solid Slip-On Sneakers ",
+    price: "Rs. 1559 ",
+    rs: 1559,
+    strikedoffprice: "Rs 2599 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url: "https://images.fashiola.in/product-list/300x450/myntra/102112062/myntra-men-relaxed-fit-cotton-linen-straight-leg-lounge-pants.webp ",
+    brand: "Marks & Spencer",
+    para: "Men Relaxed-Fit Cotton Linen Straight Leg Lounge Pants",
+    price: "Rs. 824 ",
+    rs: 824,
+    strikedoffprice: "Rs 1499 ",
+    offer: "(45% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/h_373,q_80,w_280/v1/assets/images/23349652/2023/5/23/1930ec78-be8e-4ec1-a72f-9c65644f76991684832929382LoungePants1.jpg",
+    brand: "Marks & Spencer",
+    para: "Men Relaxed-Fit Cotton Linen Straight Leg Lounge Pants",
+    price: "Rs. 399 ",
+    rs: 399,
+    strikedoffprice: "Rs 999 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/h_373,q_80,w_280/v1/assets/images/20600722/2022/11/3/d59fa722-d4e5-4540-b637-441c9a3905dd1667460520871Pyjamas1.jpg",
+    brand: "Marks & Spencer",
+    para: "Men Relaxed-Fit Cotton Linen Straight Leg Lounge Pants",
+    price: "Rs. 1999 ",
+    rs: 1999,
+    strikedoffprice: "Rs 3999 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/19913322/2022/10/28/ac613b75-affc-41cd-9aae-c4f3959e4c871666930287871-FCUK-Men-Black-Solid-Thermal-Tops-4621666930287417-1.jpg",
+    brand: "Roadster",
+    para: "Men Black Solid Thermal Tops",
+    price: "Rs. 499 ",
+    rs: 499,
+    strikedoffprice: "Rs 999 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1695702/2017/2/13/11486967407592-Levis-Men-Thermal-Tops-2671486967407294-1.jpg",
+    brand: "Roadster",
+    para: "Men Black Solid Thermal Tops",
+    price: "Rs. 499 ",
+    rs: 499,
+    strikedoffprice: "Rs 999 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1924785/2018/5/18/be8485c7-0944-4f38-8f5f-78b6e325fd201526634385328-US-Polo-Assn-Men-Khaki-Slim-Fit-Solid-Chinos-7181526634383854-1.jpg ",
+    brand: "Louis Philippe",
+    para: "Men Slim Fit Chinos ",
+    price: "Rs. 1049 ",
+    rs: 1049,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top"
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21549182/2023/1/14/9a1d245b-ff9c-450f-8ea8-0edeb48b11521673685294163KastnerMenBlackFormalBelt1.jpg",
+    brand: "Roadster",
+    para: "Men Black Artificial Leather Formal Belt",
+    price: "Rs. 383 ",
+    rs: 383,
+    strikedoffprice: "Rs 1599 ",
+    offer: "(76% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/24009970/2023/8/7/5ecc272c-8a73-482a-b92e-772534f553b11691387272284-Elite-Crafts-Men-Leather-Formal-Belt-3771691387271921-1.jpg ",
+    brand: "Roadster",
+    para: "Men Black Artificial Leather Formal Belt",
+    price: "Rs. 383 ",
+    rs: 383,
+    strikedoffprice: "Rs 1599 ",
+    offer: "(76% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url:"https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/15508234/2021/9/17/8ad4c499-4ed4-460f-9090-a27c891018d91631862674539BuckleUpMenBrownSolid1.jpg",
+    brand: "Roadster",
+    para: "Men Black Artificial Leather Formal Belt",
+    price: "Rs. 383 ",
+    rs: 383,
+    strikedoffprice: "Rs 1599 ",
+    offer: "(76% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "bottom",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21448058/2023/1/11/9ae1af27-f137-48ee-a9e5-3765225900801673433116398-HRX-by-Hrithik-Roshan-Men-Tracksuits-1281673433115654-1.jpg",
+    brand: "HRX by Hrithik Roshan",
+    para: "Men Geometric Print Tracksuit",
+    price: "Rs. 1499 ",
+    rs: 1499,
+    strikedoffprice: "Rs 3999 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22691488/2023/4/28/6847ce9c-7d60-44d4-9b4b-0702916d53aa1682662732524-HRX-by-Hrithik-Roshan-Men-Tracksuits-8141682662731955-1.jpg ",
+    brand: "HRX by Hrithik Roshan",
+    para: "Men Geometric Print Tracksuit",
+    price: "Rs. 1499 ",
+    rs: 1499,
+    strikedoffprice: "Rs 3999 ",
+    offer: "(50% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2151783/2017/12/17/11513489635756-US-Polo-Assn-Men-Navy-Blue-Colourblocked-Polo-Collar-T-shirt-3911513489635657-1.jpg ",
+    brand: "U.S. Polo Assn.",
+    para: "Printed Polo Collar Pure Cotton T-Shirt ",
+    price: "Rs. 1259 ",
+    rs: 1259,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2365445/2018/3/7/11520404661059-US-Polo-Assn-Men-Tshirts-4391520404660838-1.jpg ",
+    brand: "U.S. Polo Assn.",
+    para: "Printed Polo Collar Pure Cotton T-Shirt ",
+    price: "Rs. 1259 ",
+    rs: 1259,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://images.jdmagicbox.com/quickquotes/images_main/us-polo-assn-graphic-print-crew-neck-t-shirt-blue-172008156-tw36h.jpg",
+    brand: "U.S. Polo Assn.",
+    para: "Printed Polo Collar Pure Cotton T-Shirt ",
+    price: "Rs. 1259 ",
+    rs: 1259,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9447169/2019/5/9/1a411747-3978-4bdf-a8da-c4b38ce859d41557405997076-Louis-Philippe-Men-Navy-Blue-Self-Design-Slim-Fit-Single-Bre-1.jpg",
+    brand: "Louis Philippe",
+    para: "Men Formal Slim-Fit Single-Breasted Suit",
+    price: "Rs. 8799 ",
+    rs: 8799,
+    strikedoffprice: "Rs 10999 ",
+    offer: "(20% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2012700/2018/3/8/11520501070011-Louis-Philippe-Men-Suits-101520501069852-1.jpg",
+    brand: "Louis Philippe",
+    para: "Men Formal Slim-Fit Single-Breasted Suit",
+    price: "Rs. 8799 ",
+    rs: 8799,
+    strikedoffprice: "Rs 10999 ",
+    offer: "(20% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/21448502/2023/1/25/3df36bcf-2e59-4b1c-ba27-d43c796b1d0e1674651176852-Louis-Philippe-Men-Single-Breasted-Slim-Fit-Formal-3-Piece-S-1.jpg ",
+    brand: "Louis Philippe",
+    para: "Men Formal Slim-Fit Single-Breasted Suit",
+    price: "Rs. 8799 ",
+    rs: 8799,
+    strikedoffprice: "Rs 10999 ",
+    offer: "(20% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, {
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/8895359/2019/3/5/ab83a517-dc4c-4ffb-9e72-f173cbf6b0961551783007143-Louis-Philippe-Men-Blue-Self-Design-Slim-Fit-Single-Breasted-1.jpg",
+    brand: "Louis Philippe",
+    para: "Men Formal Slim-Fit Single-Breasted Suit",
+    price: "Rs. 8799 ",
+    rs: 8799,
+    strikedoffprice: "Rs 10999 ",
+    offer: "(20% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+},{
+    image_url: "https://assets.myntassets.com/fl_progressive/h_960,q_80,w_720/v1/assets/images/9447205/2019/5/9/d4888ff0-2466-4425-9c07-8c46a4938aa01557403964722-Louis-Philippe-Men-Beige-Solid-Slim-Fit-Formal-Single-Breast-1.jpg",
+    brand: "Louis Philippe",
+    para: "Men Formal Slim-Fit Single-Breasted Suit",
+    price: "Rs. 8799 ",
+    rs: 8799,
+    strikedoffprice: "Rs 10999 ",
+    offer: "(20% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+}, 
+{
+    image_url: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2151783/2017/12/17/11513489635756-US-Polo-Assn-Men-Navy-Blue-Colourblocked-Polo-Collar-T-shirt-3911513489635657-1.jpg ",
+    brand: "U.S. Polo Assn.",
+    para: "Printed Polo Collar Pure Cotton T-Shirt ",
+    price: "Rs. 1259 ",
+    rs: 1259,
+    strikedoffprice: "Rs 2099 ",
+    offer: "(40% OFF) ",
+    atc: "Add to Bag",
+    atw: "Add to Whishlist",
+    category: "top",
+},];
+
+
+var wishListData = JSON.parse(localStorage.getItem("wishListObj")) || []
+
+var bagData = JSON.parse(localStorage.getItem("BagListObj")) || []
+
+window.addEventListener('load', function() {
+    displayPage(productData)
+})
+
+document.getElementById('nameSort').addEventListener('change', sortNames);
+
+function sortNames() {
+    var selected = document.getElementById('nameSort').value;
+    if (selected == 'asc') {
+        productData.sort(function(a, b) {
+            if (a.brand > b.brand) return 1
+            if (a.brand < b.brand) return -1
+            return 0;
+        })
+
+        displayPage(productData);
+    } else {
+        productData.sort(function(a, b) {
+            if (a.brand > b.brand) return -1
+            if (a.brand < b.brand) return 1
+            return 0
+        })
+
+        displayPage(productData)
+    }
+}
+
+document.getElementById('priceSort').addEventListener('change', sortPrice);
+
+function sortPrice() {
+    var selected = document.getElementById('priceSort').value;
+    if (selected == 'low') {
+        productData.sort(function(a, b) {
+            return a.rs - b.rs;
+        })
+        displayPage(productData);
+    } else {
+        productData.sort(function(a, b) {
+            return b.rs - a.rs;
+        })
+        displayPage(productData)
+    }
+}
+
+document.getElementById('brandFilter').addEventListener('change', filterBrand);
+
+function filterBrand() {
+    var selected = document.getElementById('brandFilter').value
+    var newArray = productData.filter(function(element) {
+        if (element.brand == selected) {
+            return element;
+        }
+    })
+    displayPage(newArray);
+}
+
+
+function displayPage(productData) {
+
+    document.getElementById("container").innerHTML = "";
+
+    productData.map(function(elem) {
+        var box = document.createElement("div")
+        box.style.cursor = "pointer"
+
+        var img = document.createElement("img")
+        img.src = elem.image_url
+
+        var contentBox = document.createElement('div');
+        contentBox.setAttribute('class', 'contentBox')
+
+        var brand = document.createElement("h4")
+        brand.textContent = elem.brand
+
+        var productname = document.createElement("p")
+        productname.textContent = elem.para
+
+
+        var mix = document.createElement("div")
+        mix.setAttribute("class", "mixbox")
+
+
+        var price = document.createElement("p")
+        price.textContent = elem.price
+
+        var strprice = document.createElement("p")
+        strprice.textContent = elem.strikedoffprice
+        strprice.setAttribute("class", "strikep")
+
+
+        var offer = document.createElement("p")
+        offer.textContent = elem.offer
+        offer.setAttribute("class", "offerp")
+
+        mix.append(price, strprice, offer)
+
+        
+
+        var atw = document.createElement("p")
+        atw.setAttribute("class", "wishListp")
+        atw.textContent = elem.atw;
+        atw.style.cursor = "pointer"
+
+        atw.addEventListener("click", function() {
+            addToWishlist(elem)
+            atw.style.color = "green"
+            atw.innerText = "ADDED TO WISHLIST"
+        })
+
+
+      
+        var atc = document.createElement("p")
+        atc.setAttribute("class", "addToBagp")
+        atc.textContent = elem.atc;
+        atc.style.cursor = "pointer"
+
+
+        atc.addEventListener("click", function() {
+            addToBag(elem)
+            atc.innerText = "ADDED TO BAG"
+        })
+
+        contentBox.append(brand, productname, mix, atw, atc)
+
+        box.append(img, contentBox)
+
+        document.querySelector("#container").append(box);
+
+
+
+    });
+
+}
+
+
+
+function addToWishlist(element) {
+    // console.log(element)
+    wishListData.push(element)
+    localStorage.setItem("wishListObj", JSON.stringify(wishListData))
+}
+
+function addToBag(element) {
+    // console.log(element)
+    bagData.push(element)
+    localStorage.setItem("BagListObj", JSON.stringify(bagData))
+}
+
+document.getElementById('landingPage').addEventListener('click', function() {
+window.location.href = "../Landingpage/index.html"
+}) 
