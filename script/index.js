0b09b8ee5c828e92989516bdf34735cd6c6ade79
sunil8090 <sunilkumar859095@gmail.com>
2023-08-16 17:16:12 +0530

    final commit

--- script/index.js
+++ script/index.js
@@ -18,21 +18,24 @@ btn_log.addEventListener("click",function(){
     localStorage.setItem("status",JSON.stringify(false));
     btn.style.display = "block"
     btn_log.style.display = "none"
+    window.location.assign("./index.html")
 })
 
 
 //////////////////////////////////////////////////
-var rightSideIcons = document.getElementsByClassName("rightSideIcons");
+var wish_list = document.getElementById("wish_list");
+var cart_one = document.getElementById("cart_one");
 
 
-rightSideIcons[0].addEventListener("click",function(){
+
+wish_list.addEventListener("click",function(){
     var stat_s = JSON.parse(localStorage.getItem("status")) || false;
     if(!stat_s){
         window.location.assign("");
         window.location.assign("./signup/login.html");
     }
 })
-rightSideIcons[1].addEventListener("click",function(){
+cart_one.addEventListener("click",function(){
     var stat_s = JSON.parse(localStorage.getItem("status")) || false;
         if(!stat_s){
             window.location.assign("");
