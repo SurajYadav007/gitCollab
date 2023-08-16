8a029056e34576b8a4feab9b771ba48db5642ad7
ashwekkalgutkar <72547233+ashwekkalgutkar@users.noreply.github.com>
2023-08-16 15:38:26 +0530

    final code for payment and payment confirmation page

--- payment/scripts/index.js
+++ payment/scripts/index.js
@@ -1,41 +1,113 @@
-var amount = localStorage.getItem("amount");
+let price_details = JSON.parse(localStorage.getItem("order-pricing-details"));
+if (price_details) {
+  document.querySelector(".totalprice").textContent = price_details.totalMRP;
+  document.querySelector(".filldiscount").textContent =
+    "- " + price_details.discount;
+  document.querySelector(".amount_pay").textContent = price_details.totalAmount;
+}
 
-document.querySelector(".amount_pay").innerText = amount;
-
-var MRP = localStorage.getItem("MRP");
-document.querySelector(".totalprice").innerText = MRP;
-
-var discount = localStorage.getItem("discount");
-document.querySelector(".filldiscount").innerText = "- " + discount;
-
-var itemcount = localStorage.getItem("itemcount");
-document.querySelector(
-  ".pricedets"
-).innerText = `PRICE DETAILS ( ${itemcount} Items)`;
-
-// document.querySelector("#first").addEventListener("click", function () {
-//   window.location.href = "../cart/index.html";
+// document.querySelector("#btn-PO1").addEventListener("click", function () {
+//   window.location.href = "./payment-confirmation.html";
 // });
+document.querySelector("#btn-PO2").addEventListener("click", function () {
+  window.location.href = "./payment-confirmation.html";
+});
+document.querySelector("#btn-PO3").addEventListener("click", function () {
+  window.location.href = "./payment-confirmation.html";
+});
+document.querySelector("#first").addEventListener("click", function () {
+  window.location.href = "../cart/index.html";
+});
 
-// document.querySelector("#second").addEventListener("click", function () {
-//   window.location.href = "../cart/address.html";
-// });
+document.querySelector("#second").addEventListener("click", function () {
+  window.location.href = "../cart/address.html";
+});
 
-document.getElementById('logo').addEventListener('click',function(){
+document.getElementById("logo").addEventListener("click", function () {
   window.location.href = "../index.html";
 });
-document.querySelector('.giftcard-base-apply').addEventListener('click',function(){
-  
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".giftcard-base-apply");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+document
+  .querySelector(".giftcard-base-apply")
+  .addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+document.querySelector(".btn").addEventListener("click", function () {
+  console.log(price_details.totalAmount);
+  let gfcnumber = document.querySelector("#gcNumber").value;
+  let gfcpin = document.querySelector("#gcPin").value;
+
+  if (!gfcnumber || !gfcpin) {
+    alert("fill the details");
+  } else if (gfcnumber == "Masai50" && gfcpin == "123456") {
+    let totalprice = JSON.parse(price_details.totalAmount) - 50;
+    price_details.totalAmount = totalprice;
+    document.querySelector(".amount_pay").textContent = totalprice;
+    localStorage.setItem(
+      "order-pricing-details",
+      JSON.stringify(price_details)
+    );
+  }
+  closeModal();
 });
 const paymentMethods = document.querySelectorAll("#mode > div");
 const paymentDiv = document.getElementById("paymentDiv");
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+// let button1 = document.querySelector("#btn-place-order");
+let button2 = document.querySelector("#btn-place-order2");
+let button3 = document.querySelector("#btn-place-order3");
+// document
+//   .querySelector('input[id="PhonePe"]')
+//   .addEventListener("change", () => {
+//     console.log(button1.classList)
+//     button1.classList.remove("hidden");
+//     button2.classList.add("hidden");
+//     button1.classList.add("hidden");
+//   });
+document
+  .querySelector('input[id="Gpay"]')
+  .addEventListener("change", () => {
+    button2.classList.remove("hidden");
+    // button1.classList.add("hidden");
+    button3.classList.add("hidden");
+  });
+document
+  .querySelector('input[id="Cod"]')
+  .addEventListener("change", () => {
+    button3.classList.remove("hidden");
+    button2.classList.add("hidden");
+    // button1.classList.add("hidden");
+  });
+let reco = document.querySelector('#Recommended-div');
 
+document.querySelector('#py_mode1').addEventListener('click', function () {
+  console.log(reco.classList)
+  reco.classList.remove('hidden');
+})
 paymentMethods.forEach((method) => {
   method.addEventListener("click", () => {
-    let paymentdiv = document.getElementById("paymentDiv");
-    paymentdiv.innerHTML = "";
-    // console.log(method.id)
-    // Reset styles for all payment methods
     paymentMethods.forEach((m) => {
       m.querySelector("i").style.color = "#000000";
       m.querySelector("h5").style.color = "#000000";
@@ -45,75 +117,26 @@ paymentMethods.forEach((method) => {
     method.querySelector("i").style.color = "red";
     method.querySelector("h5").style.color = "red";
 
-    // // Display content in paymentDiv
-    // paymentDiv.innerHTML = `<h4>${method.querySelector('h5').textContent}</h4><p>Additional content here...</p>`;
-
     switch (method.id) {
       case "py_mode1":
-        console.log(`1`);
         // code block
-        renderRecommandPayment(paymentdiv);
+        //renderRecommandPayment(paymentdiv);
         break;
       case "py_mode2":
         // code block
-        renderCODPayment(paymentdiv);
+        // renderCODPayment(paymentdiv);
         break;
       default:
       // code block
     }
   });
 });
-renderRecommandPayment = (parentElement) => {
-  let title = document.createElement("h4");
-  title.innerHTML = "Recommended Payment Options";
-
-  const form = document.createElement("form");
-
-  const radioButton1 = document.createElement("input");
-  radioButton1.type = "radio";
-  radioButton1.name = "paymentOptions";
-
-  const label1 = document.createElement("label");
-  label1.innerHTML = "PhonePe";
-  const br1 = document.createElement("br");
-  form.append(radioButton1, label1, br1);
-
-  const radioButton2 = document.createElement("input");
-  radioButton2.type = "radio";
-  radioButton2.name = "paymentOptions";
-
-  const label2 = document.createElement("label");
-  label2.innerHTML = "GPay";
-  const br2 = document.createElement("br");
-
-  form.append(radioButton2, label2, br2);
-
-  const radioButton3 = document.createElement("input");
-  radioButton3.type = "radio";
-  radioButton3.name = "paymentOptions";
-
-  const label3 = document.createElement("label");
-  label3.innerHTML = "Cash On Delivery";
-  form.append(radioButton3, label3);
-
-  parentElement.append(title, form);
+const btn_visible = function () {
+  place_orderbtn.classList.remove("hidden");
 };
-
-renderCODPayment = (parentElement) => {
-  let title = document.createElement("h4");
-  title.innerHTML = "Cash On Delivery (Cash/UPI)";
-
-  const form = document.createElement("form");
-
-  const radioButton1 = document.createElement("input");
-  radioButton1.type = "radio";
-  radioButton1.name = "paymentOptions";
-  const label1 = document.createElement("label");
-  label1.innerHTML = "Cash on Delivery (Cash/UPI)";
-  form.append(radioButton1, label1);
-  parentElement.append(title, form);
+const btn_hide = function () {
+  place_orderbtn.classList.add("hidden");
 };
-
 const showMoreLink = document.getElementById("showMore");
 const showLessLink = document.getElementById("showLess");
 const additionalOffers = document.getElementById("additionalOffers");
@@ -129,10 +152,3 @@ showLessLink.addEventListener("click", () => {
   showMoreLink.style.display = "block";
   showLessLink.style.display = "none";
 });
-
-
-let price_details = JSON.parse(localStorage.getItem('order-pricing-details'));
-
-document.querySelector('.totalprice').textContent = price_details.totalMRP;
-document.querySelector('.filldiscount').textContent = "- " + price_details.discount;
-document.querySelector('.amount_pay').textContent = price_details.totalAmount;
